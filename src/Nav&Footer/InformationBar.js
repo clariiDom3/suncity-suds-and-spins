5c761d41a7d3ec3ca4fe41e76d3560d36c6efce1
clariiDom3 <cdominguez30@miners.utep.edu>
2024-01-11 17:02:17 -0700

    Initial commit

---
+++ src/Nav&Footer/InformationBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function InformationBar() {
+  return (
+    <div className='Info'>
+      <text>Call us</text>
+
+      <text>Location</text>
+    </div>
+  )
+}
---
+++ src/Nav&Footer/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='Nav'>
+        <text>hello</text>
+      
+    </div>
+  )
+}
